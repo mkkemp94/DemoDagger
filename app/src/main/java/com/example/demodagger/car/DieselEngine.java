066bb035f3e4875a94de4331d5459399afdff033
MKemp <mkemp@accu-time.com>
2019-09-08 12:20:23 -0400

    Inject DieselEngine horsepower at runtime.
    It is more efficient to use a custom builder to put variables into our dependency graph at runtime, because then Dagger doesn't need to create an instance of our module, and the module can remain abstract, but that's coming up.

--- app/src/main/java/com/example/demodagger/car/DieselEngine.java
+++ app/src/main/java/com/example/demodagger/car/DieselEngine.java
@@ -2,16 +2,19 @@ package com.example.demodagger.car;
 
 import android.util.Log;
 
+import javax.inject.Inject;
+
 public class DieselEngine implements Engine {
-
+    
     private static final String TAG = "Car";
-
+    
     private int mHorsePower;
-
+    
+    @Inject
     public DieselEngine(int horsePower) {
         mHorsePower = horsePower;
     }
-
+    
     @Override
     public void start() {
         Log.d(TAG, "Diesel engine started. Horsepower: " + mHorsePower);
--- app/src/main/java/com/example/demodagger/dagger/DieselEngineModule.java
+++ app/src/main/java/com/example/demodagger/dagger/DieselEngineModule.java
@@ -8,20 +8,42 @@ import dagger.Provides;
 
 @Module
 public class DieselEngineModule {
-
-    // Can also create a provides method for horsepower,
-    // but this is only necessary if we want to use this in other places as well
+    
+    /**
+     * This passes the value at runtime to a module.
+     * which puts the value into the dependency graph.
+     * You later need to provide it using a provide method.
+     * <p>
+     * Consider the following instead:
+     * <p>
+     * It is more efficient to use a custom builder.
+     * That way you can use BindsInstance to get variables into the graph at runtime.
+     * Dagger doesn't need to create an instance of the module if BindsInstance is used.
+     * <p>
+     * See PetrolEngineModule.java later
+     */
     private int mHorsepower;
-
+    
     public DieselEngineModule(int horsepower) {
         mHorsepower = horsepower;
     }
-
+    
+    /**
+     * Whenever an INTEGER is needed it can get it from this method.
+     * <p>
+     * Makes sense now to put horsepower part and engine into separate modules
+     * so we can use them independently of each other...
+     */
     @Provides
-    Engine provideEngine() {
-        return new DieselEngine(mHorsepower);
+    int provideHorsePower() {
+        return mHorsepower;
     }
-
+    
+    @Provides
+    Engine provideEngine(DieselEngine dieselEngine) {
+        return dieselEngine;
+    }
+    
     /**
      * Better performance and more concise.
      * Dagger never creates an instance of this class,
@@ -29,7 +51,7 @@ public class DieselEngineModule {
      *
      * @return a new DieselEngine
      */
-
+    
     //    @Binds // doesn't support configuration
     //    abstract Engine bindEngine(DieselEngine engine);
 }
